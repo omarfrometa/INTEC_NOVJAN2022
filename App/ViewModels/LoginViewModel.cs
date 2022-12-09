dd0b1fa3d0e70acb5608e97a33009afb12a62ae8
Omar Frometa <omar.frometa@fvtech.net>
2022-12-08 20:31:16 -0500

    Add project files.

---
+++ App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,75 @@
+﻿using Acr.UserDialogs;
+using App.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace App.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        public Command LoginCommand { get; }
+        public Command CancelCommand { get; }
+
+        #region Properties
+        private string user = string.Empty;
+        public string User
+        {
+            get => user;
+            set { 
+                SetProperty(ref user, value);
+            }
+        }
+
+        private string password = string.Empty;
+        public string Password
+        {
+            get => password;
+            set
+            {
+                SetProperty(ref password, value);
+            }
+        }
+
+        private bool rememberme;
+        public bool RememberMe
+        {
+            get => rememberme;
+            set
+            {
+                SetProperty(ref rememberme, value);
+            }
+        }
+        #endregion
+
+        public LoginViewModel()
+        {
+            LoginCommand = new Command(OnLoginClicked);
+            CancelCommand = new Command(OnCancelClicked);
+        }
+
+        private void OnCancelClicked(object obj)
+        {
+
+        }
+
+        private async void OnLoginClicked(object obj)
+        {
+            if (string.IsNullOrEmpty(User) || string.IsNullOrWhiteSpace(User))
+            {
+                await UserDialogs.Instance.AlertAsync("Por favor digite un Usuario.");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(Password) || string.IsNullOrWhiteSpace(Password))
+            {
+                await UserDialogs.Instance.AlertAsync("Por favor digite una Contraseña.");
+                return;
+            }
+
+            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+        }
+    }
+}
---
+++ App/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
